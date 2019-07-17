cd1e2b6d770c563744b625f54cfe69329de5e4fe
Alexander Riccio <test35965+learn@gmail.com>
2019-07-17 18:53:43 -0400

    wtf

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email
+        })
+    }).then((response) => {return response.json();}).then((response) => {
+        console.log("response: ");
+        console.log(response);
+        // const newElem = document.createElement("p");
+        // newElem.innerHTML = response.id;
+        // document.querySelector("body").appendChild(newElem);
+        document.body.innerHTML += `<div>${response.id}</div>`;
+    }).catch((error) => {
+        document.body.innerHTML += `<div>${error.message}</div>`;
+    });
+
+    console.log(document.body.innerHTML);
+}
